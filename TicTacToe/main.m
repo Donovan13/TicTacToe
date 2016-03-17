5d5e1968c4a701bcd022034bc7580b2dbd1daccd
Kyle <Ky@kyles-macbook-pro.local>
2016-03-17 13:52:14 -0500

    Initial Commit

---
+++ TicTacToe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Kyle on 3/17/16.
+//  Copyright © 2016 Kyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
